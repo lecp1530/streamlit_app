5c513e6b0b2b4d394e2062f754bda1176600a578
Luis Chavez <lecp1530@gmail.com>
2023-11-22 15:57:14 -0500

    Primera version de la app

---
+++ code1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+
+st.title('Mi programa en streamlit')
+st.write('Hola **como estas**')
